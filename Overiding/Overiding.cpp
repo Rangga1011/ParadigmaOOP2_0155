91eb211ff6a7409d554ea8292012a81e17cb0d51
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-28 10:33:05 +0700

    Menambahkan input baseClass

---
+++ Overiding/Overiding.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//class parent
+//tambahkan final sesudah nama class
+//untuk mencegah adanya overriding
+class baseClass
+{
+public:
+	virtual void perkenalan()
+	{
+		cout << "Hallo saya function dari base class";
+	}
+};
+
